6e062c5ef03264b699b15465c6a1337a793f31cc
katecursed99 <katejesek@gmail.com>
2026-06-10 12:58:16 -0400

    bug fix

--- outputfilter.py
+++ outputfilter.py
@@ -1,3 +1,6 @@
 def FilterOutputs(reply, priv_key, filt_mode):
     if filt_mode:
-        reply.replace(priv_key, "[REDACTED SECRET KEY]")
+        cleaned_reply = reply.replace(priv_key, "[REDACTED SECRET KEY]")
+        return cleaned_reply
+    else:
+        return reply
